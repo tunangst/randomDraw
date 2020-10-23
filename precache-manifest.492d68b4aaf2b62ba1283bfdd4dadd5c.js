23a5176a7716041184ca24ec63c0c22a4e578bc9
TRU <trunangst@gmail.com>
2020-10-23 03:37:13 -0500

    Updates

---
+++ precache-manifest.492d68b4aaf2b62ba1283bfdd4dadd5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61c091fc52e4d31b2f3063c70abc8374",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5c41a5f68e2e80bfcfd",
+    "url": "/static/css/main.04654c18.chunk.css"
+  },
+  {
+    "revision": "b4667ab265ec52bda563",
+    "url": "/static/js/2.99fe0abd.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/static/js/2.99fe0abd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5c41a5f68e2e80bfcfd",
+    "url": "/static/js/main.c157fb58.chunk.js"
+  },
+  {
+    "revision": "18e0b4a86ff7bd1f5b0a",
+    "url": "/static/js/runtime-main.4f62db56.js"
+  }
+]);
